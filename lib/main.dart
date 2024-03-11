ee39a8c6eafdf55b8ed399db6183c9db2fe6fa7e
Alvitronex <marioenriquealvarado12@gmail.com>
2024-03-10 19:58:08 -0600

    add firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:app_medicine/pages/SignupPage.dart';
 import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'pages/LoginPage.dart';
 import 'pages/SignupPage.dart';
 import 'pages/HomePage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const Main());
 }
 
